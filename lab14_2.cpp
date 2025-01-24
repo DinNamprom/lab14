d570c593cbd32c99dfddb336a77afb65779e81c4
DinNamprom <56390732+DinNamprom@users.noreply.github.com>
2025-01-24 16:12:12 +0700

    lab14-2 completed

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -20,4 +20,50 @@ int main(){
 	return 0;
 }
 
+void inputMatrix(double matrix[][N]) {
+	for (int i=0;i<N;i++) {
+		cout << "Row " << i+1 << ": ";
+		for (int j=0;j<N;j++) {
+			cin >> matrix[i][j];
+		}
+	}
+}
+
+void findLocalMax(const double MatA[][N], bool MatB[][N]) {
+	int up=0,low=N,left=0,right=N;
+
+	for (int i=0;i<N;i++) {
+		MatB[up][i] = false;
+	}
+	for (int i=0;i<N;i++) {
+		MatB[low][i] = false;
+	}
+	for (int i=0;i<N;i++) {
+		MatB[i][left] = false;
+	}
+	for (int i=0;i<N;i++) {
+		MatB[i][right] = false;
+	}
+
+	for (int j=1;j<N-1;j++) {
+		for(int k=1;k<N-1;k++) {
+			double Slag=MatA[j][k];
+			if (Slag >= MatA[j][k-1] && Slag >= MatA[j][k+1] && Slag >= MatA[j-1][k] && Slag >= MatA[j+1][k]) {
+				MatB[j][k] = true;
+			}else {
+				MatB[j][k] = false;
+			}
+		}
+	}
+}
+
+void showMatrix(const bool matrix[][N]) {
+	for (int i=0;i<N;i++) {
+		for (int j=0;j<N;j++) {
+			cout << matrix[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+
 //Do not modify source code above this line 
